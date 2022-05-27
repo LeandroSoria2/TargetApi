85b2a45f47d0f4c548947da9feee597f370196b0
Lea <87084375+LeandroSoria2@users.noreply.github.com>
2022-05-27 14:48:57 -0300

    Feature/login models (#2)
    * test
    
    * Login devise models
    
    * Add condeowners file
    
    * Modify codeowners file
    
    * Revert "Login devise models"
    
    This reverts commit 2dab4d51cd93fd1b522414820051d87ce6cf410a.
    
    * Revert "test"
    
    This reverts commit 44330236c8b8306e9a7cf62ed582872626ad7ce0.
    
    * Add user authentication, add devise token auth
    
    * Fix user fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'main#home'
 end
