df5d65c14ce8d62dc84f198ce2573df61c50c50b
Leandro Soria <leandro.soria@rootstrap.com>
2022-05-31 18:34:14 -0300

    Add annonate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,32 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  allow_password_change  :boolean          default(FALSE)
+#  email                  :string
+#  encrypted_password     :string           default(""), not null
+#  gender                 :string
+#  image                  :string
+#  lastname               :string
+#  name                   :string
+#  nickname               :string
+#  provider               :string           default("email"), not null
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  tokens                 :json
+#  uid                    :string           default(""), not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_uid_and_provider      (uid,provider) UNIQUE
+#
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
