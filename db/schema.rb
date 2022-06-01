5108c7b5b955a522a884eaa2f691920ccca408ef
Leandro Soria <leandro.soria@rootstrap.com>
2022-06-01 16:12:42 -0300

    Add email validation and gender.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_20_200840) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_01_190711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,7 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_20_200840) do
     t.string "lastname"
     t.string "nickname"
     t.string "image"
-    t.string "email"
+    t.string "email", null: false
     t.string "gender"
     t.json "tokens"
     t.datetime "created_at", null: false
