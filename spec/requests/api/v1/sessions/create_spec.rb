3b87cfcda90535edf57bee5787ed66ef4db5ac9b
Leandro Soria <leandro.soria@rootstrap.com>
2022-06-15 11:15:13 -0300

    Add test for session and add test for user

---
+++ spec/requests/api/v1/sessions/create_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+
+RSpec.describe 'POST api/v1/users/sign_in', type: :request do
+  subject { post new_user_session_path, params: params, as: :json }
+
+  let(:password) { 'password' }
+  let(:user) { create(:user, password: password) }
+  let(:params) do
+    {
+      user:
+        {
+          email: user.email,
+          password: password
+        }
+    }
+  end
+
+  context 'with correct params' do
+    before do
+      subject
+    end
+
+    it_behaves_like 'there must not be a Set-Cookie in Header'
+    it_behaves_like 'does not check authenticity token'
+
+    it 'returns success' do
+      expect(response).to be_successful
+    end
+
+    it 'returns the user' do
+      expect(json[:user][:id]).to eq(user.id)
+      expect(json[:user][:email]).to eq(user.email)
+      expect(json[:user][:username]).to eq(user.username)
+      expect(json[:user][:uid]).to eq(user.uid)
+      expect(json[:user][:provider]).to eq('email')
+      expect(json[:user][:first_name]).to eq(user.first_name)
+      expect(json[:user][:last_name]).to eq(user.last_name)
+      expect(json[:user][:gender]).to eq(user.gender)
+    end
+
+    it 'returns a valid client and access token' do
+      token = response.header['access-token']
+      client = response.header['client']
+      expect(user.reload.valid_token?(token, client)).to be_truthy
+    end
+  end
+
+  context 'with incorrect password' do
+    let(:params) do
+      {
+        user: {
+          email: user.email,
+          password: 'wrong_password!'
+        }
+      }
+    end
+
+    it 'return errors upon failure' do
+      subject
+
+      expect(response).to be_unauthorized
+      expected_response = {
+        error: 'Invalid login credentials. Please try again.'
+      }.with_indifferent_access
+      expect(json).to eq(expected_response)
+    end
+  end
+
+  context 'with incorrect email' do
+    let(:params) do
+      {
+        user: {
+          email: 'mail@mail.com',
+          password: user.password
+        }
+      }
+    end
+
+    it 'return errors upon failure' do
+      subject
+
+      expect(response).to be_unauthorized
+      expected_response = {
+        error: 'Invalid login credentials. Please try again.'
+      }.with_indifferent_access
+      expect(json).to eq(expected_response)
+    end
+  end
+end
