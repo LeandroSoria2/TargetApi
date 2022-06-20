7f55cb47e15db3cf4b057b07dfadb393bb23aebf
Leandro Soria <leandro.soria@rootstrap.com>
2022-06-20 16:29:54 -0300

    Change migration gender and test destroy_spec

--- db/migrate/20220603145240_change_user_gender_enum.rb
+++ db/migrate/20220603145240_change_user_gender_enum.rb
@@ -1,6 +1,5 @@
 class ChangeUserGenderEnum < ActiveRecord::Migration[7.0]
-  def up
-    execute "ALTER TABLE users ALTER gender DROP DEFAULT;"
-    change_column :users, :gender, :integer, using: 'gender::integer'
+  def change
+    change_column :users, :gender, :integer
   end
 end
