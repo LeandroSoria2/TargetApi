4092b0d130f84865cc7e9a69af2d1d03280e857f
Leandro Soria <leandro.soria@rootstrap.com>
2022-06-20 16:29:54 -0300

    Change migration gender and test destroy_spec

--- spec/requests/api/v1/sessions/destroy_spec.rb
+++ spec/requests/api/v1/sessions/destroy_spec.rb
@@ -1,23 +1,26 @@
 describe 'DELETE aapi/v1/auth/sign_out', type: :request do
   let(:user) { create(:user) }
 
+  subject { delete destroy_api_v1_user_session_path, headers: headers, as: :json } 
   context 'with a valid token' do
+    let(:headers) { auth_headers }
     it 'returns a successful response' do
-      delete destroy_api_v1_user_session_path, headers: auth_headers, as: :json
+      subject
       expect(response).to have_http_status(:success)
     end
 
     it 'decrements the amount of user tokens' do
       headers = auth_headers
       expect {
-        delete destroy_api_v1_user_session_path, headers: headers, as: :json
+        subject
       }.to change { user.reload.tokens.size }.by(-1)
     end
   end
 
   context 'without a valid token' do
+    let(:headers) {}
     it 'returns not found response' do
-      delete destroy_api_v1_user_session_path, headers: {}, as: :json
+      subject
       expect(response).to have_http_status(:not_found)
     end
   end
