f77dcbef023c4343963f6622ad4bde4fb83f0fab
Leandro Soria <leandro.soria@rootstrap.com>
2022-06-23 17:26:42 -0300

    Fix migration

--- db/migrate/20220603145240_change_user_gender_enum.rb
+++ db/migrate/20220603145240_change_user_gender_enum.rb
@@ -1,5 +1,5 @@
 class ChangeUserGenderEnum < ActiveRecord::Migration[7.0]
   def change
-    change_column :users, :gender, :integer
+    change_column :users, :gender, :integer, using: 'gender::integer'
   end
 end
