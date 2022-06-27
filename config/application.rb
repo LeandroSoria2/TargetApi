0328d6d32577cd4182474e103474e0e5fd7c1aaa
Leandro Soria <leandro.soria@rootstrap.com>
2022-06-27 14:19:56 -0300

    fix ruby-version

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Target
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
+    config.add_autoload_paths_to_load_path = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
