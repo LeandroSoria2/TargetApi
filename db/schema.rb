0d844c890c420cde1ac0004ccd82216e27de013a
Leandro Soria <leandro.soria@rootstrap.com>
2022-07-09 21:29:35 -0300

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2022_07_01_173650) do
-=======
 ActiveRecord::Schema[7.0].define(version: 2022_07_04_184644) do
->>>>>>> fa45389 (Add CRUD topics and tests)
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
