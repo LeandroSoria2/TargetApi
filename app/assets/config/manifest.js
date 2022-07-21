4676a83b4ff0ac0360ec917bc8e5fb3d044340fe
Leandro Soria <leandro.soria@rootstrap.com>
2022-07-21 12:55:18 -0300

    Fix manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link apitome/application.css
+//= link apitome/highlight_themes/default.css
+//= link apitome/application.js
