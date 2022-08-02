063b127d5ca0961130a00d03ee4b4f97ff0cde7d
Leandro Soria <leandro.soria@rootstrap.com>
2022-08-02 14:40:43 -0300

    Add target migration and model

---
+++ spec/models/target_spec.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: targets
+#
+#  id         :bigint           not null, primary key
+#  topic_id   :bigint           not null
+#  user_id    :bigint           not null
+#  title      :string           not null
+#  radius     :integer          not null
+#  longitude  :decimal(, )      not null
+#  latitude   :decimal(, )      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_targets_on_topic_id  (topic_id)
+#  index_targets_on_user_id   (user_id)
+#
+require 'rails_helper'
+
+RSpec.describe Target, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
