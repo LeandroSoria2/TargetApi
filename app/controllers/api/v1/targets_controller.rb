82c0e3c5d32be2eb8a636b430536aff4a4de2469
Leandro Soria <leandro.soria@rootstrap.com>
2022-08-09 17:40:32 -0300

    Add delete controller and rspec test

--- app/controllers/api/v1/targets_controller.rb
+++ app/controllers/api/v1/targets_controller.rb
@@ -9,10 +9,23 @@ module Api
         @target = current_user.targets.create!(target_params)
       end
 
+      def destroy
+        target.destroy!
+        head :no_content
+      rescue ActiveRecord::RecordNotFound => e
+        render json: {
+          error: e.to_s
+        }, status: :not_found
+      end
+
       private
 
       def target_params
         params.require(:target).permit(:title, :radius, :longitude, :latitude, :topic_id)
+      end
+
+      def target
+        @target ||= current_user.targets.find(params[:id])
       end
     end
   end
---
+++ spec/requests/api/v1/targets/destroy_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'DELETE api/v1/targets/:id' do
+  let(:user) { create(:user) }
+  let(:headers) { auth_headers }
+  let!(:target) { create(:target, user: user) }
+  let(:target_id) { target.id }
+
+  subject { delete api_v1_target_path(id: target_id), as: :json, headers: headers }
+
+  context 'when user sends own target id' do
+    it 'returns ok' do
+      subject
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'deletes the target' do
+      expect {
+        subject
+      }.to change { Target.count }.by(-1)
+    end
+  end
+
+  context 'when user sends someone elses target id' do
+    let(:target_id) { 1237 }
+
+    it 'returns not_found' do
+      subject
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+end
