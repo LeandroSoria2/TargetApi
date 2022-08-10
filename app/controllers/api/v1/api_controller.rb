7b596c986e96b10896f6e2582200ec507fc82c54
Leandro Soria <leandro.soria@rootstrap.com>
2022-08-10 12:23:03 -0300

    Fix pr comments

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,7 +3,7 @@ module Api
     class ApiController < ApplicationController
       include DeviseTokenAuth::Concerns::SetUserByToken
       before_action :authenticate_user!
-      protect_from_forgery with: :null_session
+      protect_from_forgery unless: -> { request.format.json? }
     end
   end
 end
