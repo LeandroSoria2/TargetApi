b569639c56587f3bbb79ddffb6cf4200e84f8b04
Leandro Soria <leandro.soria@rootstrap.com>
2022-08-16 14:23:47 -0300

    Fix pr comments

--- spec/requests/api/v1/targets/index_spec.rb
+++ spec/requests/api/v1/targets/index_spec.rb
@@ -6,25 +6,28 @@ describe 'GET api/v1/targets', type: :request do
   let(:parsed_response) { JSON.parse(response.body) }
 
   subject { get api_v1_targets_path, as: :json, headers: headers }
+
   context 'when user has targets' do
     let(:num_of_targets) { Faker::Number.between(from: 1, to: 10) }
     let!(:targets) { create_list(:target, num_of_targets, user: user) }
 
-    before { subject }
-
     it 'returns status code ok' do
+      subject
+
       expect(response).to have_http_status(:success)
     end
 
     it 'returns all the user targets' do
+      subject
+
       expect(parsed_response.size).to eq(num_of_targets)
     end
   end
 
   context 'when user does not have targets' do
-    before { subject }
-
     it 'returns an empty array' do
+      subject
+
       expect(parsed_response).to be_empty
     end
   end
@@ -32,9 +35,9 @@ describe 'GET api/v1/targets', type: :request do
   context 'when user is not signed in' do
     let(:headers) { {} }
 
-    before { subject }
-
     it 'returns unauthorized' do
+      subject
+
       expect(response).to have_http_status(:unauthorized)
     end
   end
