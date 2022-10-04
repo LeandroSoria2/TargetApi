88fd265a7c4eb65d022ba4f582e0d3c5ecaa0c88
Leandro Soria <leandro.soria@rootstrap.com>
2022-10-04 15:13:51 -0300

    Fix space line

--- db/migrate/20220909172624_create_matches.rb
+++ db/migrate/20220909172624_create_matches.rb
@@ -3,7 +3,8 @@ class CreateMatches < ActiveRecord::Migration[7.0]
     create_table :matches do |t|
       t.belongs_to :target, null: false
       t.references :compatible_target, index: true, foreign_key: { to_table: :targets }
+
       t.timestamps
-    end  
+    end
   end
 end
