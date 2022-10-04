c2cc097bec20648cc2bdeda80d36c4d1a95b51e1
Leandro Soria <leandro.soria@rootstrap.com>
2022-10-04 16:37:37 -0300

    Add match service, and add rspec test for match

--- app/services/targets/create_service.rb
+++ app/services/targets/create_service.rb
@@ -11,13 +11,30 @@ module Targets
     def call
       raise MaxAllowTargetsError unless targets.count < MAX_ALLOWED_TARGETS
 
-      targets.create!(params)
+      Target.transaction do
+        target = targets.create!(params)
+        find_match(target)
+        target
+      end
     end
 
     private
 
     def targets
       @targets ||= current_user.targets
+    end
+
+    def find_match(target)
+      compatible_target = Target.within(target.radius, origin: [target.latitude, target.longitude])
+                                .where(topic_id: target.topic_id)
+                                .where.not(user_id: target.user_id)
+                                &.first
+
+      create_match(target, compatible_target) if compatible_target.present?
+    end
+
+    def create_match(target, compatible_target)
+      Match.create!(target: target, compatible_target: compatible_target)
     end
   end
 end
