3663946d1c94517e75916b24ba0347ab502f31f7
Leandro Soria <leandro.soria@rootstrap.com>
2022-10-12 15:28:33 -0300

    fix pr comments

--- spec/queries/target_query_spec.rb
+++ spec/queries/target_query_spec.rb
@@ -3,87 +3,78 @@ require 'rails_helper'
 describe TargetQuery do
   describe '#compatible_target' do
     subject do
-      described_class.new.compatible_target(target)
+      described_class.new.compatible_target(new_target)
     end
     let!(:user) { create(:user) }
     let!(:topic) { create(:topic) }
     let(:topic_id) { topic.id }
 
-    let!(:target) do
-      create(:target,
-             radius: 20,
-             latitude: 60,
-             longitude: -60,
-             topic_id: topic_id,
-             matched: false)
+    let(:new_target) do
+      build(:target,
+            radius: 25,
+            latitude: 60,
+            longitude: -60,
+            topic_id: topic_id)
     end
 
     context 'when there are targets in range' do
-      let!(:new_target) do
-        create(:target,
-               radius: 25,
-               latitude: 60,
-               longitude: -60,
-               topic_id: topic_id)
-      end
-      context 'when there are targets with same topic' do
-        let!(:new_target) do
-          create(:target,
-                 radius: 25,
-                 latitude: 60,
-                 longitude: -60,
-                 topic_id: topic_id)
-        end
-        context 'when there are targets with matched false' do
-          let!(:new_target) do
+      context 'when there are targets with the same topic' do
+        context 'when there are targets without a match' do
+          let!(:target) do
             create(:target,
-                   radius: 25,
+                   radius: 20,
                    latitude: 60,
                    longitude: -60,
                    topic_id: topic_id,
                    matched: false)
           end
 
-          it 'does return complatible target' do
-            expect(subject).to eq(new_target)
+          it 'returns a complatible target' do
+            expect(subject).to eq(target)
           end
         end
-        context 'when there are targets with matched ture' do
-          let!(:new_target) do
+
+        context 'when there are not targets without a match' do
+          let!(:target) do
             create(:target,
-                   latitude: 22.5,
-                   longitude: -22.5,
+                   radius: 20,
+                   latitude: 60,
+                   longitude: -60,
                    topic_id: topic_id,
                    matched: true)
           end
 
-          it 'return non matched targets' do
+          it 'does not return a compatible target' do
             expect(subject).to be nil
           end
         end
       end
 
-      context 'when the target has not the same topic' do
-        let!(:new_target) do
+      context 'when there are not targets with the same topic' do
+        let!(:target) do
           create(:target,
-                 latitude: 22.5,
-                 longitude: -22.5)
+                 radius: 20,
+                 latitude: 60,
+                 longitude: -60,
+                 matched: false)
         end
-        it 'does not return targets matched' do
+
+        it 'does not return a compatible target' do
           expect(subject).to be nil
         end
       end
     end
 
     context 'when there are not targets in range' do
-      let!(:new_target) do
+      let!(:target) do
         create(:target,
-               latitude: 22.5,
-               longitude: -22.5,
-               topic_id: topic_id)
+               radius: 20,
+               latitude: 200,
+               longitude: -200,
+               topic_id: topic_id,
+               matched: false)
       end
-
-      it 'return non matched targets in range' do
+      it 'does not return a compatible target' do
         expect(subject).to be nil
       end
     end
